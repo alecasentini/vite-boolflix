152c94470be224ba4b9e15a314a50b5ddf393716
alecasentini <ale.casentini@gmail.com>
2023-05-09 14:30:58 +0200

    Creazione componenti Header e Main

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,24 @@
+<script >
+export default {
+    name: "HeaderComp"
+}
+</script>
+
+<template>
+    <header class="w-100 d-flex justify-content-center align-items-center">
+
+        <h1>Boolfix</h1>
+
+    </header>
+</template>
+
+<style lang="scss">
+header {
+    background-color: black;
+    height: 100px;
+
+    h1 {
+        color: red;
+    }
+}
+</style>
---
+++ src/components/MainComp.vue
@@ -0,0 +1,15 @@
+<script >
+export default {
+    name: "MainComp"
+}
+</script>
+
+<template>
+    <main class="w-100 d-flex justify-content-center align-items-center">
+
+
+
+    </main>
+</template>
+
+<style lang="scss"></style>
